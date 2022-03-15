6d770985673342a04616642a703f5ad87715aac6
VBobell <vinicius.leite@gamersclub.com.br>
2022-03-15 10:12:59 -0300

    fix: change storybook refs

--- packages/ui/react/.storybook/main.js
+++ packages/ui/react/.storybook/main.js
@@ -2,4 +2,10 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: '@storybook/react',
+  refs: {
+    '@vbobel/design-system-styles-ui': {
+      title: '@vbobel/design-system-styles-ui',
+      url: 'https://vbobell.github.io/design-system',
+    },
+  },
 };
