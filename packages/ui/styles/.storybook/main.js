8b21db39e8f1cb0612c77654b3d49d6661e4804e
VBobell <vinicius.leite@gamersclub.com.br>
2022-03-15 17:03:14 -0300

    chore: add storybook addon viewport

--- packages/ui/styles/.storybook/main.js
+++ packages/ui/styles/.storybook/main.js
@@ -2,7 +2,12 @@ const path = require('path');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-postcss'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-postcss',
+    '@storybook/addon-viewport',
+  ],
   framework: '@storybook/html',
   core: {
     builder: 'webpack5',
