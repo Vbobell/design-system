8d976c6761afb79a479d4b79c71a502db9ad2fc2
VBobell <vinicius.leite@gamersclub.com.br>
2022-03-15 16:52:19 -0300

    feat: add webpack5 to storybook

--- packages/ui/react/.storybook/main.js
+++ packages/ui/react/.storybook/main.js
@@ -2,6 +2,9 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
   refs: {
     '@vbobel/design-system-styles-ui': {
       title: '@vbobel/design-system-styles-ui',
--- packages/ui/styles/.storybook/main.js
+++ packages/ui/styles/.storybook/main.js
@@ -2,25 +2,15 @@ const path = require('path');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-postcss'],
   framework: '@storybook/html',
+  core: {
+    builder: 'webpack5',
+  },
   refs: {
     '@vbobel/design-system-react-ui': {
       title: '@vbobel/design-system-react-ui',
       url: 'https://vbobell.github.io/design-system/react',
     },
-  },
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: [
-        {
-          loader: 'postcss-loader',
-        },
-      ],
-      include: path.resolve(__dirname, '../'),
-    });
-
-    return config;
   },
 };
