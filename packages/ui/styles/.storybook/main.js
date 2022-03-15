7ba991b6cc8b8a59f0488c2f11684731e71acbd0
VBobell <vinicius.leite@gamersclub.com.br>
2022-03-15 09:53:10 -0300

    fix: change storybook main to package styles

--- packages/ui/styles/.storybook/main.js
+++ packages/ui/styles/.storybook/main.js
@@ -4,6 +4,12 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/html',
+  refs: {
+    '@vbobel/design-system-react-ui': {
+      title: '@vbobel/design-system-react-ui',
+      url: 'https://vbobell.github.io/design-system/react',
+    },
+  },
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.css$/,
